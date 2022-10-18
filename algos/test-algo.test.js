bd20c9c16441fac48101de732a7ce167f9dcaafc
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-10-18 09:35:17 -0600

    added small algo

--- algos/test-algo.test.js
+++ algos/test-algo.test.js
@@ -1,9 +1,14 @@
 const {
-    sum
+    sum,
+    mult
 } = require('./test-algo')
 
 
 
 test('Sould add 1 + 3 to equal 4', () => {
     expect(sum(1,3)).toBe(4)
+})
+
+test("Should multiply 5 times 5 to equal 25", () => {
+    expect(mult(5,5)).toBe(25)
 })
