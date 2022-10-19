48ae6f2a1977c3164103b083b65575264357e36d
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-10-19 07:56:40 -0600

    vowelCount algo

--- algos/test-algo.js
+++ algos/test-algo.js
@@ -6,7 +6,22 @@ const mult = (a,b) => {
     return a * b
 }
 
+const vowelCount = (str) => {
+    // I need to define the Vowels
+    let vowels = ["a","e","i","o","u"];
+    // I need a counter to count the vowels
+    let count = 0;
+    //Loop through the string to count the vowels
+    str.split(' ').forEach((elem) => {
+        let last = elem[elem.length - 1].toLowerCase()
+        if(vowels.includes(last))
+        count++
+    })
+    return count
+}
+
 module.exports = {
     sum,
-    mult
+    mult,
+    vowelCount
 }
--- algos/test-algo.test.js
+++ algos/test-algo.test.js
@@ -1,6 +1,7 @@
 const {
     sum,
-    mult
+    mult,
+    vowelCount
 } = require('./test-algo')
 
 
@@ -11,4 +12,8 @@ test('Sould add 1 + 3 to equal 4', () => {
 
 test("Should multiply 5 times 5 to equal 25", () => {
     expect(mult(5,5)).toBe(25)
+})
+
+test("Should count the words that end in a vowels", () => {
+    expect(vowelCount('I want to code now')).toBe(3)
 })
